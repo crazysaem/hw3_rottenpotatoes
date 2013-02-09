4479dd17422e82a75b7e2c084785e68d3ba2b03b
saasbook <saasbook@saasbook.(none)>
2013-02-09 06:53:18 -0800

    step 1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,31 @@
 # Add a declarative step here for populating the DB with movies.
 
+Before do
+  load File.dirname(__FILE__) + '/../../db/seeds.rb'
+end
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    # puts movie
+    condition = Hash.new()
+    condition["title"] = movie["title"]
+    condition["rating"] = movie["rating"]
+    # puts condition
+    mov = Movie.find(:first, :conditions =>  condition)
+    if (mov != nil)
+      if (mov.release_date.to_s == ActiveSupport::TimeZone['UTC'].parse(movie["release_date"]).to_s)
+        assert true
+        puts "true"
+      else
+        assert false
+      end
+    else
+      assert false
+    end
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +34,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
